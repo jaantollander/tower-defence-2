6c843808bce629196554412998ed66fa9ab7ca85
jaan <jaan.tollander@gmail.com>
2017-11-10 18:49:56 +0200

    Base classes

---
+++ src/game.h
@@ -0,0 +1,64 @@
+#ifndef TOWER_DEFENCE_2_GAME_H
+#define TOWER_DEFENCE_2_GAME_H
+
+
+class Game {
+    /*
+     * Game class contains the game map, enemies, towers and game stats.
+     * This class is also responsible for the implementation of the main
+     * game loop.
+     *
+     * Attributes:
+     *
+     * - time
+     * - game_map
+     * - towers
+     * - enemies
+     *
+     * Method:
+     *
+     * - update: The main game loop. Updates the game time, positions of the enemies,
+     *   make towers shoot etc.
+     *
+     */
+
+};
+
+
+class GameStats {
+    /*
+     * Class that contains information about the stats such as how much money the
+     * player has and the score of the player.
+     *
+     * Attributes:
+     *
+     * - score
+     * - money
+     *
+     * Methods:
+     *
+     */
+
+};
+
+
+class GameMap {
+    /*
+     * The map of the game. Contains information about the path that enemies follow
+     * and where towers can be placed.
+     *
+     * Attributes:
+     *
+     *
+     * Method:
+     *
+     * - load_from_file: Load game map from file
+     * - validate: Validate that the map
+     *
+     */
+
+};
+
+
+
+#endif //TOWER_DEFENCE_2_GAME_H
---
+++ src/tower.h
@@ -0,0 +1,29 @@
+#ifndef TOWER_DEFENCE_2_TOWER_H
+#define TOWER_DEFENCE_2_TOWER_H
+
+
+class TowerBase {
+    /*
+     * Abstract base class for all towers.
+     *
+     * Attributes:
+     *
+     * - Damage
+     * - Attack range
+     * - Attack speed
+     * - Cost
+     * - Health
+     *
+     * Methods:
+     *
+     * - Enemies in range
+     * - Enemy targeting policy, should we attack enemy that is first or last or
+     *   maybe has least health?
+     * - Upgrade
+     * - Repair
+     *
+     */
+
+};
+
+#endif //TOWER_DEFENCE_2_TOWER_H
