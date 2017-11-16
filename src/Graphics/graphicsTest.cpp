5667c5b2242cdc6b6b9ccb3e4bb6f2d1e51f421f
Roni Hytonen <roni.hytonen@aalto.fi>
2017-11-16 15:58:18 +0200

    Updated grapihics stuff

--- src/Graphics/graphicsTest.cpp
+++ src/Graphics/graphicsTest.cpp
@@ -1,7 +1,8 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <iostream>
-#include <stdlib.h>   // For rand()
+#include <stdlib.h>     // For rand()
+#include <math.h>       // For round()
 
 
 // This is a test-level code for SFML C++ graphics.
@@ -9,6 +10,64 @@
 // Then compile the code as: g++ -c graphicsTest.cpp; 
 //		g++ graphicsTest.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system
 // And run as: ./sfml-app
+
+// Iterate through drawables and draw them on screen
+// 	OBS: DRAWING HAPPENS IN THE ORDER THEY ARE DEFINED!
+void drawDrawables(sf::RenderWindow &window, std::vector<sf::Drawable *> drawables)
+{
+    std::vector<sf::Drawable *>::iterator it = drawables.begin();
+    for( ; it != drawables.end(); it++ )
+    {
+        window.draw(**it);
+    }
+}
+
+// Iterate through map and draw the tiles
+void drawTiles(sf::RenderWindow &window, std::vector< std::vector<int> > &map,
+                        int tileAmount, sf::Vector2f tileSize)
+{
+    for (int i = 0; i < tileAmount; ++i) {
+        for (int j = 0; j < tileAmount; ++j) {
+            sf::RectangleShape tmpTile(tileSize);
+            tmpTile.setPosition(j*tileSize.x, i*tileSize.y); // OBS j,i !
+            tmpTile.setOutlineColor(sf::Color::White);
+            tmpTile.setOutlineThickness(2);
+
+            switch(map[i][j])
+            {
+                case 0: tmpTile.setFillColor(sf::Color::Green);
+                    break;
+                case 1: tmpTile.setFillColor(sf::Color::White);
+                    break;
+                case 2: tmpTile.setFillColor(sf::Color::Red);
+                    break;
+            }
+            window.draw(tmpTile);
+        }
+    }
+}
+
+
+void drawCreatures(sf::RenderWindow &window,
+                   std::vector<sf::Vector3f> creatures, sf::Vector2f tileSize)
+{
+    for (int i = 0; i < creatures.size(); ++i) {
+        sf::CircleShape tmpCreature(std::min(tileSize.x, tileSize.y) / 2);
+            tmpCreature.setPosition(creatures[i].x, creatures[i].y); // OBS j,i !
+
+        switch((int) creatures[i].z) {
+            case 1:
+                tmpCreature.setFillColor(sf::Color::Blue);
+                break;
+            case 2:
+                tmpCreature.setFillColor(sf::Color::Yellow);
+                break;
+        }
+
+        window.draw(tmpCreature);
+    }
+}
+
 
 int main()
 {
@@ -20,10 +79,11 @@ int main()
 // ------------------------------------------------------ //
 
 		// Create a window where stuff is drawn - 4:3 aspect ratio
-    sf::RenderWindow window(sf::VideoMode(windowSize.x, windowSize.y,
-                                          3), "Graphics test");
+    sf::RenderWindow window(sf::VideoMode(windowSize.x, windowSize.y), "Graphics test");
+        window.setKeyRepeatEnabled(false);
     
-    	// Store all of the drawables to a vector 
+    	// Store all of the drawables to a vector
+        // TODO: Functions to create and append each part of UI
     std::vector<sf::Drawable *> drawables;
 
     	// Create two rectangles for the basis of map and status parts
@@ -44,7 +104,7 @@ int main()
 // ------------------------------------------------------ //
 
 		// Create text fields that are needed
-		
+
 	sf::Font font;	// A font type has to be downloaded
 		font.loadFromFile("FreeMono.ttf");
 		
@@ -77,19 +137,32 @@ int main()
  		// Colour the tiles according to map info
 
     // Create a random map
-    int randomMap[tileAmount][tileAmount];
+    std::vector< std::vector<int> > randomMap(tileAmount);
     for (int i = 0; i < tileAmount; ++i) {
         for (int j = 0; j < tileAmount; ++j) {
-            randomMap[i][j] = rand() % 3; // Values[0,1,2]
-            std::cout << randomMap[i][j] << ' ';
+            randomMap[i].push_back(rand() % 3);
+//            std::cout << randomMap[i][j] << ' ';
         }
-        std::cout << std::endl;
+//        std::cout << std::endl;
     }
+
+
+// ------------------------------------------------------ //
+
+        // Create some creatures to be drawn on tiles
+    std::vector<sf::Vector3f> creatures;
+        creatures.push_back(sf::Vector3f(60, 60, 1 ));
+        creatures.push_back(sf::Vector3f(300, 120, 2 ));
 
 // ------------------------------------------------------ //
 	    // Draw (and update) the objects to the screen
+
+        // Start a clock
+    sf::Clock clock;
+        // While window has not been closed, keep on going
     while (window.isOpen())
     {
+            // Sniff for closing window
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -97,42 +170,33 @@ int main()
                 window.close();
         }
 
-        window.clear();
-  
-  			// Iterate through drawables and draw them on screen
-  			// 	OBS: DRAWING HAPPENS IN THE ORDER THEY ARE DEFINED!
-  		std::vector<sf::Drawable *>::iterator it = drawables.begin();
-  		for( ; it != drawables.end(); it++ )
-  		{
-  			window.draw(**it);
-  		}
-
-            // Iterate through randomMap and draw tiles
-        for (int i = 0; i < tileAmount; ++i) {
-            for (int j = 0; j < tileAmount; ++j) {
-                sf::RectangleShape tmpTile(tileSize);
-                tmpTile.setPosition(j*tileSize.x, i*tileSize.y); // OBS j,i !
-                tmpTile.setOutlineColor(sf::Color::White);
-                tmpTile.setOutlineThickness(2);
-
-                switch(randomMap[i][j])
-                {
-                    case 0: tmpTile.setFillColor(sf::Color::Green);
-                            break;
-                    case 1: tmpTile.setFillColor(sf::Color::White);
-                            break;
-                    case 2: tmpTile.setFillColor(sf::Color::Red);
-                            break;
-                }
-                window.draw(tmpTile);
-//                std::cout << randomMap[i][j] << ' ';
-            }
-//            std::cout << std::endl;
+            // Sniff for mouse LeftClick
+        if( sf::Mouse::isButtonPressed(sf::Mouse::Left) )
+        {
+            creatures[1].x = sf::Mouse::getPosition(window).x;
         }
 
-                
+            // What's our in-game time?
+        sf::Time elapsedTime = clock.restart();
+
+            // Window has to be cleaned every time to avoid overlap
+        window.clear();
+
+            // Bunch of drawing functions
+        drawDrawables(window, drawables);
+
+        drawTiles(window, randomMap, tileAmount, tileSize);
+
+        drawCreatures(window, creatures, tileSize);
+
+            // Update creatures' positions
+        float displacement = 100 * elapsedTime.asSeconds(); // pix/s * s = pix
+        creatures[1].y = fmod(creatures[1].y + displacement, windowSize.y);
+
         window.display();
+
     }
 
     return 0;
 }
+
