5b0c0a1753b282ea588de0b4fde3d8af822b8d1d
Tero <tero@git@version.aalto.fi>
2017-11-21 17:53:12 +0200

    Updated tower class

--- src/objects/tower.cpp
+++ src/objects/tower.cpp
@@ -1,10 +1,19 @@
 #include "tower.h"
+#include "../map.h"
 
-TowerBase::TowerBase(int x, int y, int radius, int health, int damage,
-                     int attack_range, int attack_speed, int cost,
-                     int repair_cost, int upgrade_cost) :
-        Object(x, y, radius, 0),
-        Combat(health, damage, attack_range, attack_speed),
-        m_cost(cost),
-        m_repair_cost(repair_cost),
-        m_upgrade_cost(upgrade_cost) {}
+GameMap getMap() { return load_game_map("/maps/testmap1") }
+//this is just for test!
+
+TowerBase::TowerBase(int x, int y) : m_object(x, y, 0, 0) { }
+TowerBase::~TowerBase() { }
+
+TowerBase* TowerBase::upgrade() {
+    if (m_upgradable) {
+        GameMap m = getMap();   // Towers will need to know where
+                                // they are for this to work!
+                                // TODO
+        m.get_tile(m_object.x(), m_object.y()) -> update_tower(m_upgrades_to);
+        return m.get_tile(m_object.x(), m_object.y()) -> tower();
+    }
+}
+
